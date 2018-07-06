8ad9d7b533c6e51b8475dceb7097645d2b4d64f1
Steven Lum <stevielum1@gmail.com>
2018-07-06 15:02:36 -0700

    Finish phase 1 of actve_record_lite

--- active_record_lite/lib/00_attr_accessor_object.rb
+++ active_record_lite/lib/00_attr_accessor_object.rb
@@ -1,5 +1,15 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      getter = "@" + name.to_s
+      define_method(name.to_sym) do
+        return instance_variable_get(getter.to_sym)
+      end
+      
+      setter = name.to_s + "="
+      define_method(setter.to_sym) do |value|
+        return instance_variable_set(getter.to_sym, value)
+      end
+    end
   end
 end
--- active_record_lite/lib/01_sql_object.rb
+++ active_record_lite/lib/01_sql_object.rb
@@ -5,26 +5,53 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    if @columns
+      @columns
+    else
+      columns = DBConnection.execute2(<<-SQL)
+        SELECT
+          *
+        FROM
+          #{self.table_name}
+        SQL
+      @columns = columns.first.map(&:to_sym)
+    end
   end
 
   def self.finalize!
+    self.columns.each do |column|
+      define_method(column.to_sym) do
+        attributes[column]
+      end
+      
+      setter = column.to_s + "="
+      define_method(setter.to_sym) do |value|
+        attributes[column] = value
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name.tableize
   end
 
   def self.table_name
-    # ...
+    @table_name ||= self.to_s.tableize
   end
 
   def self.all
-    # ...
+    data = DBConnection.execute(<<-SQL)
+      SELECT
+        #{self.table_name}.*
+      FROM
+        #{self.table_name}
+      SQL
+      
+    self.parse_all(data)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map{|datum| self.new(datum)}
   end
 
   def self.find(id)
@@ -32,11 +59,17 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+      attr_name_sym = attr_name.to_sym
+      columns = self.class.columns
+      raise "unknown attribute '#{attr_name}'" unless columns.include?(attr_name_sym)
+      setter = attr_name.to_s + "="
+      self.send(setter.to_sym, value)
+    end  
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
