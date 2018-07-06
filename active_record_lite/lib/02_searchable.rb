b8b4abaee4e9a992ce28fd16ca5cc26a2485cb2e
Steven Lum <stevielum1@gmail.com>
2018-07-06 15:38:35 -0700

    Finish phase 2

--- active_record_lite/lib/02_searchable.rb
+++ active_record_lite/lib/02_searchable.rb
@@ -3,10 +3,20 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.keys.map(&:to_s).map{|key| "#{key} = ?"}.join(" AND ")
+    attr_values = params.values
+    data = DBConnection.execute(<<-SQL, *attr_values)
+    SELECT
+      *
+    FROM
+      #{self.table_name}
+    WHERE
+      #{where_line}
+    SQL
+    data.map { |datum| self.new(datum) }
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
